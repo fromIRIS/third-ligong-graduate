102c0ee4dfd9c2a4aa40a045aab2b9aa4bbce950
南洋 <417611525@qq.com>
2017-05-01 16:53:33 +0800

    feat(第八页):

--- app/index.js
+++ app/index.js
@@ -1,10 +1,64 @@
 require('./index.less');
 
+require('script-loader!zepto');
+require('../resource/libs/fullpage');
+
+var utils = require('./utils');
 
 
 
+function PageControll () {
+
+}
+PageControll.prototype = {
+  init: function () {
+    var self = this;
+
+    self.preloading();
+    self.pages();
+  },
+  pages: function () {
+    
+  },
+  preloading: function () {
+    var self = this;
+    var $loadingArea = $('.j_LoadingArea');
+    var $loadingBar = $('.j_LoadingBar');
+    var $page1Title = $('.j_Page1Title');
+    var $loadingStarter = $('.j_LoadingStarter');
+
+    var preLoadingImglist = [require('../resource/images/1-loading.png')];
+
+    utils.fireImage(preLoadingImglist[0], $loadingArea[0], function () {
+      fakeMoveLoading();
+    })
 
 
+
+    function fakeMoveLoading () {
+      var timer = null;
+      var count = 0;
+      timer = setInterval(function () {
+        count++;
+        $loadingBar.width($loadingBar.width() + $loadingBar.parent().width()/10);
+        if (count === 7) {
+          clearInterval(timer);
+        }
+      }, 1000)
+
+      $(window).on('load', function () {
+        clearInterval(timer);
+        $loadingBar.width($loadingBar.parent().width());
+        $page1Title.addClass('tada');
+        $loadingStarter.addClass('active');
+      }) 
+    }
+  }
+}
+
+
+new PageControll().init();
+ 
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,14 @@ switch(process.env.npm_lifecycle_event) {
     config = merge(common, 
       parts.clean(PATHS.build),
       parts.setupSourceMapForBuild(),
-      parts.setupCSS(PATHS.app),
+      parts.setupJs(),
+      parts.loadImages({
+        options: {
+          limit: 8870,
+          name: './resource/images/[name].[ext]',
+        },
+      }),
+      parts.setupCSS(),
       parts.setFreeVariable('process.env.NODE_ENV', 'production'),
       parts.minify()
       );
@@ -40,7 +47,15 @@ switch(process.env.npm_lifecycle_event) {
   default: 
     config = merge(common, 
       parts.setupSourceMapForDev(),
-      parts.devServer(), 
-      parts.setupCSS(PATHS.app));
+      parts.devServer(),
+      parts.loadImages({
+        options: {
+          limit: 8870,
+          name: './resource/images/[name].[ext]',
+        },
+      }),
+      parts.setupJs(),
+      parts.setupCSS());
+      
 }
 module.exports = config;
